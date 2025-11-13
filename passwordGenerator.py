71515a30e2fee714fed72d702b852e839952c9e0
90duse <duseibnuduse1212@gmail.com>
2025-11-13 16:47:19 +0300

    un commented the easy level

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -9,18 +9,16 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Easy Level
-# password = ""
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
-#
-# for char in range(0, nr_symbols):
-#     password += random.choice(symbols)
-#
-# for char in range(0, nr_numbers):
-#     password += random.choice(numbers)
-#
-# print(password)
+#Easy Level
+password = ""
+for char in range(0, nr_letters):
+    password += random.choice(letters)
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+for char in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
 
 # Hard level
 password_List = []
