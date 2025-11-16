a4d729be3379bf42d15b3267dcffbf7ccc9e45ce
90duse <duseibnuduse1212@gmail.com>
2025-11-16 18:38:11 +0300

    added password cracker program

--- cipherScript/passwordcrack.py
+++ cipherScript/passwordcrack.py
@@ -5,6 +5,7 @@ createdPassword = int(input("Create your ZAAD password "))
 correct_password = createdPassword
 print(" Now let me crack your password in seconds: watch me cook")
 time.sleep(2)
+#this time function will give a time to see the above print message it is not nessessary for the program
 for found in range(10000):
     print(f"Cracking Your Password {found}")
     if found == correct_password:
