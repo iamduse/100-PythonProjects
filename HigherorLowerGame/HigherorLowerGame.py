1517dbee78e4b8411050af02d292bc70d6575cf9
90duse <duseibnuduse1212@gmail.com>
2025-11-22 13:18:01 +0300

    added the final higher and lower game and also created a coffe mechine folder for the next project

--- HigherorLowerGame/HigherorLowerGame.py
+++ HigherorLowerGame/HigherorLowerGame.py
@@ -1,42 +1,64 @@
+from game_data import data
 import random
-import game_data
-#TODO 1: Create a welcome message to the game
-print("Play Compare Game")
-#section A read the name and the description
-#Pick random dictionary than use that dictionary to fetch the keys in the dictionaries
-section_a = random.choice(game_data.data)
-name = section_a["name"]
-description =  section_a["description"]
-follower_a = section_a["follower_count"]
-print(f"COMPARE A: {name}, {description}")
-print("\n"*1)
-print("VS")
-print("\n"*1)
-
-section_b = random.choice(game_data.data)
-name = section_b["name"]
-description =  section_b["description"]
-follower_b =  section_b["follower_count"]
-print(f"COMPARE B: {name}, {description}")
 
 
-# create a compare function
-def compare(A,B):
-    if follower_a > follower_b:
-        return "a win"
+def format_data(account):
+    """Takes the account data and returns the printable format."""
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_desc}, from {account_country}"
+
+
+def check_answer(user_guess, a_followers, b_followers):
+    """Take a user's guess and the follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return user_guess == "a"
     else:
-        return "B win"
+        return user_guess == "b"
 
 
-should_continue = True
-while should_continue:
-    choice = input("Who has more followers type 'a' or 'b': ")
-    if choice:
-        result = compare(section_a, section_b)
-        print(result)
-        break
+score = 0
+game_should_continue = True
+# Generate a random account from the game data
+account_b = random.choice(data)
 
+# Make the game repeatable.
+while game_should_continue:
 
+    # Making account at position B become the next account at position A.
+    account_a = account_b
+    account_b = random.choice(data)
+
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    #print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+
+    # Ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # Clear the screen
+    print("\n" * 20)
+    #print(logo)
+
+    # - Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    # Check if user is correct.
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    # Give user feedback on their guess.
+    # score keeping.
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}.")
+        game_should_continue = False
 
 
 
