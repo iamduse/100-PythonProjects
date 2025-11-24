e5014225768c5653ddd8c97cbeb8a7bdc3558dc2
90duse <duseibnuduse1212@gmail.com>
2025-11-24 16:25:55 +0300

    added some turtle project

--- Turtle/cirle.py
+++ Turtle/cirle.py
@@ -3,29 +3,18 @@ import random
 
 
 jimy = Turtle()
-jimy.shape("classic")
-jimy.color("Blue")
-jimy.pensize(6)
-
-# create random walk with random colors
+jimy.shape("circle")
 colors = [
     "red", "blue", "green", "yellow", "purple", "orange", "pink",
     "cyan", "magenta", "lime", "turquoise", "gold", "violet",
     "indigo", "coral", "salmon", "plum", "brown", "navy", "maroon",
     "olive", "teal", "skyblue", "khaki", "crimson"
 ]
-directions = [ 0, 90, 180, 270 ]
-for _ in range(200):
-    jimy.forward(30)
-    jimy.setheading(random.choice(directions))
-    jimy.speed(0)
-    jimy.color(random.choice(colors))
 
 
-
-
-
-
+jimy.color(random.choice(colors))
+jimy.circle(100)
+jimy.speed("fastest")
 
 
 
