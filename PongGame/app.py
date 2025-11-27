637b1d239e536419ba08ff254deae1ab049fa045
90duse <duseibnuduse1212@gmail.com>
2025-11-27 14:25:46 +0300

    created and finished the stick class and the screen set up in app file

--- PongGame/app.py
+++ PongGame/app.py
@@ -1,15 +1,33 @@
-from turtle import Turtle,Screen
+from turtle import Screen
+from stick_class import  Stick
+import time
+
 
 
 screen = Screen()
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
+screen.tracer(0) # turnOff the animation
+
+right_stick = Stick((370,0))
+left_stick = Stick((-380,0))
+
+
+
+screen.listen()
+screen.onkey(right_stick.up,"Up")
+screen.onkey(right_stick.down,"Down")
+screen.onkey(left_stick.up,"q")
+screen.onkey(left_stick.down,"w")
 
 
 
 
 
-
+game_on = True
+while game_on:
+    screen.update() # updates the screen after turtle is created
+    time.sleep(0.1)
 
 
 
---
+++ PongGame/stick_class.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+
+class Stick(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.up()
+        self.down()
+        self.create_stick()
+        self.goto(position)
+
+
+    def create_stick(self):
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+
+
+
+
+    def up(self):
+        y_cor = self.ycor() + 20
+        self.goto(self.xcor(), y_cor)
+
+    def down(self):
+        y_cor = self.ycor() - 20
+        self.goto(self.xcor(), y_cor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
