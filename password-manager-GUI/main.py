b5d6bcf163b7c597e53077db2813b307e2534b51
90duse <duseibnuduse1212@gmail.com>
2025-12-11 13:54:21 +0300

    adjusted the code spaces

--- password-manager-GUI/main.py
+++ password-manager-GUI/main.py
@@ -11,28 +11,22 @@ def generate_pass():
     nr_letters = random.randint(8, 10)
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
-
-
-
+    #List comprehensive concept
     password_symbol = [random.choice(symbols) for _ in range(nr_symbols)]
     password_letter = [random.choice(letters) for _ in range(nr_letters)]
     password_number = [random.choice(numbers) for _ in range(nr_numbers)]
 
     password_list = password_number + password_symbol + password_letter
     random.shuffle(password_list)
-
     password = "".join(password_list)
     password_input.insert(0, password)
-
-    #print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
-
-
+    # validate the inputs before saving
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="OOPS", message="Website and Password can not be empty")
     else:
@@ -44,16 +38,12 @@ def save():
                 # email_input.delete(0, END)
                 password_input.delete(0, END)
 
-
 # ---------------------------- UI SETUP ------------------------------- #
-
 window =Tk()
 window.title("Password Generator")
 window.config(padx=50, pady=50)
 
-
 # canvas
-
 canvas = Canvas(width=200 , height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image =logo_image)
@@ -67,7 +57,6 @@ email_label.grid(row=2 ,column=0)
 password_label = Label(text="password: ")
 password_label.grid(row=3, column=0)
 
-
 #inputs
 website_input = Entry(width=38)
 website_input.grid(row=1 , column=1, columnspan=2)
@@ -77,7 +66,6 @@ email_input.grid(row=2 , column=1, columnspan=2 )
 email_input.insert(0 , "example@gmail.com")
 password_input = Entry(width=21, )
 password_input.grid(row=3 , column=1,)
-
 
 #buttons
 gen_button = Button(text="Generate Password", command=generate_pass)
